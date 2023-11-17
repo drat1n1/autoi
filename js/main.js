57ccdb6f1551fb5f3dabb4340551e5fd972f4dd9
min <rtminy@gmail.com>
2023-11-17 17:12:35 +0900

    ADD

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-let currentIndex = 0;
-const slides = document.querySelector('.slider').children;
-const totalSlides = slides.length;
-
-function moveSlider(direction) {
-  if (direction === 'left') {
-    currentIndex = (currentIndex === 0) ? totalSlides - 1 : currentIndex - 1;
-  } else {
-    currentIndex = (currentIndex === totalSlides - 1) ? 0 : currentIndex + 1;
-  }
-  const position = -currentIndex * 1000; // 1000px는 슬라이더의 너비입니다.
-  document.querySelector('.slider').style.transform = `translateX(${position}px)`;
-}
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
